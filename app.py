e048ca97c647f028a153196fe68894fdc5dfbec6
jakeTran42 <jake.tranehs@gmail.com>
2019-10-12 17:20:54 -0700

    modified app

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ markov_chain = markov.main()
 @app.route("/")
 def home():
     generate_sentence = markov_chain.generate_sentence(25)
-    return render_template("main.html", sentence=generate_sentence)
-    # return generate_sentence
+    # return render_template("main.html", sentence=generate_sentence)
+    return generate_sentence
 
 if __name__ == "__main__":
     app.run(debug=True)
